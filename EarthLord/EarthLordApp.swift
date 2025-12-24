4798e2500e72449456eaa54f2f2d1be72e452231
xinyao <bhzbtxy@163.com>
2025-12-24 14:59:00 +0800

    Initial Commit

---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,32 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by 昕尧 on 2025/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EarthLordApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ EarthLordTests/EarthLordTests.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordTests.swift
+//  EarthLordTests
+//
+//  Created by 昕尧 on 2025/12/24.
+//
+
+import Testing
+@testable import EarthLord
+
+struct EarthLordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
